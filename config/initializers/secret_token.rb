452ba99394cc59c791363630fd5831e89a7ed3d8
mak <marketlogger@gmail.com>
2011-01-07 10:27:59 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '9e2fdec2f234d3ddc0a3b60c980b9ca88086b291352043caf712ac2182e1b5be9ff993b08f79c969811b07f4aa850e21a399d331d70d477145d10fbdd510d6a0'
